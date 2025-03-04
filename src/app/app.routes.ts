399ff89b50bdea8ac6723eaced7a2f76aa345538
Ram <ramiro.vargas@jala.university>
2025-03-04 16:03:51 -0400

    fix(pages): moved features to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login/login.component';
-import { RegisterComponent } from './features/register/register/register.component';
-import { HomeComponent } from './features/home/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 
 export const routes: Routes = [
